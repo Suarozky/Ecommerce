0f98b644d5b9b1cb358a37653f5abeaa235fb859
Brayansimp <Brayansuarez2809@gmail.com>
2025-01-25 10:13:21 -0500

    <REF>

--- e-commerce-test/src/app/api/products/route.ts
+++ e-commerce-test/src/app/api/products/route.ts
@@ -1,9 +1,17 @@
-import { NextResponse } from 'next/server';
-import { PRODUCTS } from '@/constants/products';
+import { NextResponse } from "next/server";
+import { PRODUCTS } from "@/constants/products";
 
 export async function GET() {
-  // Simulate network delay
-  await new Promise(resolve => setTimeout(resolve, 5000));
-  
-  return NextResponse.json(PRODUCTS);
+  try {
+    // Simulate network delay
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+
+    if (!PRODUCTS) {
+      throw new Error("Products not found");
+    }
+
+    return NextResponse.json(PRODUCTS);
+  } catch (error) {
+    return NextResponse.json({ error: error}, { status: 500 });
+  }
 }
--- e-commerce-test/src/lib/products.ts
+++ e-commerce-test/src/lib/products.ts
@@ -1,11 +1,10 @@
 export async function getProducts() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products`,
-    {}
-  );
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`);
 
   if (!res.ok) {
-    throw new Error("Failed to fetch products");
+    const errorMessage = await res.text();
+    console.error(`Failed to fetch products: ${errorMessage}`);
+    throw new Error(`Failed to fetch products, Status: ${res.status}`);
   }
 
   return res.json();
