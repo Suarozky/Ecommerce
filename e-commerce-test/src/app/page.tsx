1bda42da33d6510fa5d200fe8920954831995140
Brayansimp <Brayansuarez2809@gmail.com>
2025-01-25 10:25:00 -0500

    <REF>

--- e-commerce-test/src/app/page.tsx
+++ e-commerce-test/src/app/page.tsx
@@ -1,18 +1,18 @@
 // app/page.tsx
-import { ProductList } from "@/components/ProductList";
+
 import { SearchSort } from "@/components/SearchSort";
-import { getProducts } from "@/lib/products";
+
 
 
 
 export default async function Home() {
-  const products = await getProducts();
+
 
   return (
     <div className="min-h-screen bg-[#dbebfa]">
       <main className="container mx-auto px-4 pt-24 pb-12">
         <SearchSort />
-        <ProductList initialProducts={products} />
+
       </main>
     </div>
   );
--- e-commerce-test/src/lib/products.ts
+++ e-commerce-test/src/lib/products.ts
@@ -1,10 +1,11 @@
 export async function getProducts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/products`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/products`,
+    {}
+  );
 
   if (!res.ok) {
-    const errorMessage = await res.text();
-    console.error(`Failed to fetch products: ${errorMessage}`);
-    throw new Error(`Failed to fetch products, Status: ${res.status}`);
+    throw new Error("Failed to fetch products");
   }
 
   return res.json();
