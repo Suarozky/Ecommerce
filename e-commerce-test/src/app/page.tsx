feed47b7e4d0489348a697fb22fccf5199372ad6
Brayansimp <Brayansuarez2809@gmail.com>
2025-02-07 19:41:02 -0500

    <FIX>

--- e-commerce-test/src/app/page.tsx
+++ e-commerce-test/src/app/page.tsx
@@ -2,8 +2,7 @@
 import { ProductList } from "@/components/ProductList";
 import { SearchSort } from "@/components/SearchSort";
 import { getProducts } from "@/lib/products";
-
-
+import { Suspense } from "react";
 
 export default async function Home() {
   const products = await getProducts();
@@ -11,8 +10,10 @@ export default async function Home() {
   return (
     <div className="min-h-screen bg-[#dbebfa]">
       <main className="container mx-auto px-4 pt-24 pb-12">
-        <SearchSort />
-        <ProductList initialProducts={products} />
+        <Suspense fallback="Loading products...">
+          <SearchSort />
+          <ProductList initialProducts={products} />
+        </Suspense>
       </main>
     </div>
   );
